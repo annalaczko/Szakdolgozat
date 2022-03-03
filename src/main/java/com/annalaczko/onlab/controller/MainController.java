adaf14ef0597b406252daf21779e0175e8e19231
annalaczko <annalaczko.maps@gmail.com>
2022-03-03 11:27:17 +0100

    1.

--- src/main/java/com/annalaczko/onlab/controller/MainController.java
+++ src/main/java/com/annalaczko/onlab/controller/MainController.java
@@ -1,13 +1,21 @@
 package com.annalaczko.onlab.controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
 import javafx.scene.control.MenuBar;
 import javafx.scene.input.InputEvent;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 import java.util.ResourceBundle;
 
@@ -17,10 +25,42 @@ public class MainController implements Initializable
     private MenuBar menuBar;
 
     @FXML
+    private VBox vBox;
+
+    @FXML
+    private Circle robot;
+
+    @FXML
     private void handleExitAction(final ActionEvent event)
     {
         Platform.exit();
     }
+
+    Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
+
+        double deltaX = 2;
+        double deltaY = 2;
+
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            robot.setLayoutX(robot.getLayoutX() + deltaX);
+            robot.setLayoutY(robot.getLayoutY() + deltaY);
+
+            Bounds bounds = vBox.getBoundsInLocal();
+            boolean rightBorder = robot.getLayoutX() >= (bounds.getMaxX() - robot.getRadius());
+            boolean leftBorder = robot.getLayoutX() <= (bounds.getMinX() + robot.getRadius());
+            boolean bottomBorder = robot.getLayoutY() >= (bounds.getMaxY() - robot.getRadius());
+            boolean topBorder = robot.getLayoutY() <= (bounds.getMinY() + robot.getRadius());
+
+            if (rightBorder || leftBorder) {
+                deltaX *= -1;
+            }
+            if (bottomBorder || topBorder) {
+                deltaY *= -1;
+            }
+        }
+    }));
+
 
     /**
      * Handle action related to input (in this case specifically only responds to
@@ -53,6 +93,8 @@ public class MainController implements Initializable
     @Override
     public void initialize(java.net.URL arg0, ResourceBundle arg1) {
         menuBar.setFocusTraversable(true);
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
 
     }
 }
--- src/main/java/com/annalaczko/onlab/model/Robot.java
+++ src/main/java/com/annalaczko/onlab/model/Robot.java
@@ -6,15 +6,8 @@ public class Robot {
     private double speed=1; //pixel per milliseconds
 
     public void move (double distance, double degree ){
-        int iteration= (int) Math.round(distance/speed);
-        for (int i=0; i<iteration;i++){
-            location.setX(location.getX()+Math.cos(Math.toRadians(degree))*speed);
-            location.setY(location.getY()+Math.sin(Math.toRadians(degree))*speed);
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        location.setX(location.getX()+Math.cos(Math.toRadians(degree))*speed);
+        location.setY(location.getY()+Math.sin(Math.toRadians(degree))*speed);
+
     }
 }
